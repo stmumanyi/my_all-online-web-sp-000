420b183bbad61313c57fffc5104cc1ca7d77ad69
Simba Mumanyi <simbamumanyi@gmail.com>
2020-08-10 13:36:18 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,17 +1,2 @@
 require 'pry'
 
-def my_all?(collection)
-  block_return_values = []
-  i = 0 
-  while i < collection.length
-  block_return_values << yield(collection[i])
-  yield(collection[i])
-  i = i + 1
-end
- if block_return_values.include?(false)
-   false
- else 
-   true
-end
-
-end
